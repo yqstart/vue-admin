edf7865b9bd674516f618dc79b23179813e39010
yqstart <yqstart@outlook.com>
2023-02-28 11:43:39 +0800

    优化路由

--- router/src/main.ts
+++ router/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
+import router, { setupRouter } from "./router";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const init = async () => {
+    const app = createApp(App)
+    setupRouter(app)
+    // 定义异步，路由处理完成再挂载组件
+    await router.isReady()
+    app.mount('#app')
+}
+
+init().then(r => {})
