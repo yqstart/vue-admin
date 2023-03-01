119395d3645ccd05d00da50c76c3498e698b3d66
yqstart <yqstart@outlook.com>
2023-03-01 11:23:31 +0800

    router: login响应式

--- router/src/views/auth/login.vue
+++ router/src/views/auth/login.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="bg-slate-300 h-screen flex justify-center items-center">
+  <div
+    class="bg-slate-300 h-screen flex justify-center items-start md:items-center p-5"
+  >
     <div
-      class="w-[720px] bg-red-600 grid grid-cols-2 bg-white rounded-lg shadow-md overflow-hidden"
+      class="w-[720px] bg-red-600 md:grid grid-cols-2 bg-white rounded-lg shadow-md overflow-hidden translate-y-1/3 md:translate-y-0"
     >
       <div class="p-6">
         <h2 class="text-center text-gray-700 text-2xl mt-10">会员登录</h2>
@@ -17,18 +19,14 @@
             class="login-input mt-4"
           />
         </div>
-        <button
-          class="login-button mt-6"
-        >
-          登录
-        </button>
+        <button class="login-button mt-6">登录</button>
         <div class="flex gap-2 justify-center mt-5">
           <a href="" class="text-sm text-gray-700">网站首页</a>
           <a href="" class="text-sm text-gray-700">会员注册</a>
           <a href="" class="text-sm text-gray-700">找回密码</a>
         </div>
       </div>
-      <div>
+      <div class="hidden md:block">
         <img src="/image/login.jpg" class="h-96 w-full object-cover" />
       </div>
     </div>
@@ -38,10 +36,10 @@
 <script setup></script>
 
 <style scoped lang="scss">
-  .login-input {
-    @apply border border-gray-200 placeholder:text-xs w-full rounded-sm py-1 px-2 outline-none focus:ring-2 ring-offset-2 ring-violet-600 duration-300 focus:border-white;
-  }
-  .login-button {
-    @apply bg-violet-800 text-white w-full py-2 rounded-md hover:bg-violet-500 duration-300
-  }
+.login-input {
+  @apply border border-gray-200 placeholder:text-xs w-full rounded-sm py-1 px-2 outline-none focus:ring-2 ring-offset-2 ring-violet-600 duration-300 focus:border-white;
+}
+.login-button {
+  @apply bg-violet-800 text-white w-full py-2 rounded-md hover:bg-violet-500 duration-300;
+}
 </style>
