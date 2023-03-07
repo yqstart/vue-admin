a3e7aaef59cdab5abdbe5e399f7ee4a8a9e71808
yqstart <yqstart@outlook.com>
2023-03-07 17:31:15 +0800

    打包优化

--- router/vite.config.ts
+++ router/vite.config.ts
@@ -3,15 +3,32 @@ import alias from "./vite/alias";
 import parseEnv from "./vite/utils";
 import * as process from "process";
 import setupVitePlugins from "./vite/plugins";
+import { visualizer } from "rollup-plugin-visualizer";
 export default ({ command, mode }: ConfigEnv) => {
   const isBuild = command === "build";
   const root = process.cwd();
   // 提取配置文件
   const env = parseEnv(loadEnv(mode, root));
   return {
-    plugins: setupVitePlugins(isBuild, env),
+    plugins: [setupVitePlugins(isBuild, env), visualizer()],
     resolve: {
       alias,
+    },
+    build: {
+      rollupOptions: {
+        emptyOutDir: true,
+        output: {
+          manualChunks(id: string) {
+            if (id.includes("node_modules")) {
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+          },
+        },
+      },
     },
   };
 };
--- router/vite/plugins/index.ts
+++ router/vite/plugins/index.ts
@@ -1,9 +1,18 @@
 import vue from "@vitejs/plugin-vue";
 import { Plugin } from "vite";
 import setupMockPlugin from "./mock";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 const setupVitePlugins = (isBuild: boolean, env: ViteEnv) => {
-  const plugins: Plugin[] = [vue()];
+  const plugins: Plugin[] = [
+    vue(),
+    AutoImport({ resolvers: [ElementPlusResolver()] }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ];
   plugins.push(setupMockPlugin(isBuild));
   return plugins;
 };
